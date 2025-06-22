5b238119e541c768fa562dd32f2a237f09b89b7f
Teoslip <tjachiet@gmail.com>
2025-06-22 18:13:13 +0200

    Add checkWin checkTie and takencell logicù

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function GameBoard() {
     }
 
     const fillCell = (row, column, token) => {
+        if (board[row][column].getValue() !== 0) {
+
+        }
         board[row][column].addValue(token);
     };
 
@@ -16,11 +19,51 @@ function GameBoard() {
         return board.map((row) => row.map((cell) => cell.getValue()));
     };
 
+    const checkEmptyCell = (row, column) => {
+        if (board[row][column].getValue() === 0) return true;
+        else return false;
+    };
+
+    const checkWin = () => {
+        function isWinning(x, y, z, a, b, c) {
+            return board[a][x].getValue() === board[b][y].getValue() && board[a][x].getValue() === board[c][z].getValue() && board[a][x].getValue() !== 0;
+        }
+        //Check for row win
+        for (let i = 0; i < boardSize; i++) {
+            if (isWinning(0, 1, 2, i, i, i)) {
+                return true;
+            }
+        }
+
+        //Check for column win
+        for (let i = 0; i < boardSize; i++) {
+            if (isWinning(i, i, i, 0, 1, 2)) {
+                return true;
+            }
+        }
+
+        //Check for diagonals
+        if (board[0][0].getValue() === board[1][1].getValue() && board[0][0].getValue() === board[2][2].getValue() && board[0][0].getValue() !== 0) return true;
+        if (board[0][2].getValue() === board[1][1].getValue() && board[0][2].getValue() === board[2][0].getValue() && board[0][2].getValue() !== 0) return true;
+
+    };
+
+    const checkTie = () => {
+        for (let i = 0; i < boardSize; i++) {
+            for (let j = 0; j < boardSize; j++) {
+                if (board[i][j].getValue() === 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
     const resetBoard = () => {
         board.map((row) => row.map((cell) => cell.addValue(0)))
     };
 
-    return { displayBoard, fillCell, resetBoard };
+    return { displayBoard, fillCell, resetBoard, checkTie, checkWin, checkEmptyCell };
 }
 
 function Cell() {
@@ -40,6 +83,8 @@ function GameFlow(name1 = 'player1', name2 = 'player2') {
     const players = [{ name: name1, token: 1 }, { name: name2, token: 2 }];
     let currentPlayer = players[0];
 
+    console.log(`It's ${currentPlayer.name}'s turn to play !`);
+
     const changeTurn = () => {
         if (currentPlayer === players[0]) currentPlayer = players[1];
         else currentPlayer = players[0];
@@ -49,13 +94,25 @@ function GameFlow(name1 = 'player1', name2 = 'player2') {
 
     const playRound = (row, column) => {
 
-        board.fillCell(row, column, currentPlayer.token);
+        if (board.checkEmptyCell(row, column)) {
+            board.fillCell(row, column, currentPlayer.token);
+        } else {
+            console.log(`Cell is already filled ! Choose another one.`);
+            console.log(`It's ${currentPlayer.name}'s turn to play !`);
+            return;
+        }
 
-        console.log(board.displayBoard());
-        changeTurn();
+        if (board.checkWin()) {
+            console.log(`Game Over ! ${currentPlayer.name} wins !`);
+        } else if (board.checkTie()) {
+            console.log(`No space left, it's a tie !`);
+        } else {
+            console.log(board.displayBoard());
+            changeTurn();
+        }
     };
 
-    return {playRound}
+    return { playRound }
 
 }
 
