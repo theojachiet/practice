16420f001bb38d62e5f3788d8688626a9e5e907b
Teoslip <tjachiet@gmail.com>
2025-06-22 16:17:38 +0200

    Add playround logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function GameBoard() {
     const boardSize = 3;
-    let board = [[],[],[]];
+    let board = [[], [], []];
 
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
@@ -20,11 +20,11 @@ function GameBoard() {
         board.map((row) => row.map((cell) => cell.addValue(0)))
     };
 
-    console.log(displayBoard());
+    return { displayBoard, fillCell, resetBoard };
 }
 
 function Cell() {
-    let value = 1;
+    let value = 0;
 
     const getValue = () => value;
 
@@ -32,11 +32,33 @@ function Cell() {
         value = token;
     };
 
-    return {getValue, addValue};
+    return { getValue, addValue };
 }
 
-function GameFlow() {
+function GameFlow(name1 = 'player1', name2 = 'player2') {
+    const board = GameBoard();
+    const players = [{ name: name1, token: 1 }, { name: name2, token: 2 }];
+    let currentPlayer = players[0];
+
+    const changeTurn = () => {
+        if (currentPlayer === players[0]) currentPlayer = players[1];
+        else currentPlayer = players[0];
+
+        console.log(`It's ${currentPlayer.name}'s turn to play !`);
+    };
+
+    const playRound = (row, column) => {
+
+        board.fillCell(row, column, currentPlayer.token);
+
+        console.log(board.displayBoard());
+        changeTurn();
+    };
+
+    return {playRound}
 
 }
 
-GameBoard();
+const board = GameBoard();
+
+const game = GameFlow();
